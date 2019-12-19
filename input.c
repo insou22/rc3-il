75396e1c9e1ff42219095f1d0d742a6a9efa4041
insou22 <zac.kologlu@gmail.com>
2019-12-19 13:30:56 +1100

    small cleanup

--- input.c
+++ input.c
@@ -13,34 +13,41 @@
 #define save_pos ((void*)0xD9FF00)
 #define save_save ((void*)0x1100000)
 
-void entry() {
-    if((down_buttons & BTN_ACTION) == BTN_ACTION) {
-        // reload planet
-        if((pressed_buttons & BTN_RELOAD) == BTN_RELOAD) {
-            // reload save
-            if((down_buttons & BTN_ALT) == BTN_ALT) {
-                if((down_buttons & BTN_UP) || *(int*)save_save == 0) {
-                    // prevent stack overflow from giga memcpy
-                    for(i = 0; i < 0x200000; i += 0x8000) {
-                        memcpy((void*)((int)save_save + i), (void*)((int)savedata_buf + i), 0x8000);
-                    }
-                }
+// macros
+#define down(button) ((down_buttons & (button)) == (button))
+#define pressed(button) ((pressed_buttons & (button)) == (button))
 
-                perform_load(0, save_save);
+
+void entry() {
+    if (!down(BTN_ACTION)) {
+        return;
+    }
+
+    // reload planet
+    if (pressed(BTN_RELOAD)) {
+        // reload save
+        if (down(BTN_ALT)) {
+            if (down(BTN_UP) || * (int*) save_save == 0) {
+                // prevent stack overflow from giga memcpy
+                for (i = 0; i < 0x200000; i += 0x8000) {
+                    memcpy((void*) ((int) save_save + i), (void*) ((int) savedata_buf + i), 0x8000);
+                }
             }
-            // just reload planet
-            else {
-                destination_planet = current_planet;
-                should_load = 1;
-            }
+
+            perform_load(0, save_save);
         }
-        // save pos
-        else if((pressed_buttons & BTN_SAVE) == BTN_SAVE) {
-            memcpy(save_pos, &player_pos, 0x20);
+        // just reload planet
+        else {
+            destination_planet = current_planet;
+            should_load = 1;
         }
-        // load pos
-        else if((pressed_buttons & BTN_LOAD) == BTN_LOAD) {
-            memcpy(&player_pos, save_pos, 0x20);
-        }
+    }
+    // save pos
+    else if (pressed(BTN_SAVE)) {
+        memcpy(save_pos, &player_pos, 0x20);
+    }
+    // load pos
+    else if (pressed(BTN_LOAD)) {
+        memcpy(&player_pos, save_pos, 0x20);
     }
 }
